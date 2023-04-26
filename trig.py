66c02b34d07e4a85f9fcaf17a2b1b048fb49a65d
RY <126905892+RY-05@users.noreply.github.com>
2023-04-26 15:49:18 +0100

    Update trig.py

--- trig.py
+++ trig.py
@@ -8,9 +8,12 @@ def deg_to_rad(theta):
     return rad
 
 
+# h value
+small_val = 10 ** -3
+
 # arbitrary value to account for zero division error
 # can be any number and will not affect the graphs' shapes
-arb_val = 0
+arb_val = -30
 
 # u represents the first x value taken
 u1 = -360
@@ -32,60 +35,103 @@ lim = 30
 x = np.linspace(u, v, vals)
 
 # sine graph
+plt.title("f(x) = sin(x), f'(x) = cos(x)")
 y = np.sin(x)
+dy_dx = [(m.sin(x[i] + small_val) - m.sin(x[i])) /small_val \
+         for i in range(u1 + 360, v1 + 360, step)]
+
 plt.plot(x, y)
+plt.plot(x, dy_dx)
+
 plt.show()
 
 # cosine graph
+plt.title("f(x) = cos(x), f'(x) = -sin(x)")
 y = np.cos(x)
+dy_dx = [(m.cos(x[i] + small_val) - m.cos(x[i])) / small_val \
+         for i in range(u1 + 360, v1 + 360, step)]
+
 plt.plot(x, y)
+plt.plot(x, dy_dx)
+
 plt.show()
 
 # tangent graph
+plt.title("f(x) = tan(x), f'(x) = sec²(x)")
 y = np.tan(x)
+dy_dx = [(m.tan(x[i] + small_val) - m.tan(x[i])) / small_val \
+         for i in range(u1 + 360, v1 + 360, step)]
+
 plt.plot(x, y)
+plt.plot(x, dy_dx)
 plt.ylim(-lim, lim)
 plt.show()
 
 # secant graph
+plt.title("f(x) = sec(x), f'(x) = tan(x)sec(x)")
 y = []
 for i in range(u1, v1, step):
     a = deg_to_rad(i)
-
     if m.cos(a) == 0:
         y.append(arb_val)
-
     else:
         y.append(1 / m.cos(a))
 
+dy_dx = []
+for i in range(u1, v1, step):
+    a = deg_to_rad(i)
+    if (m.cos(a + small_val) == 0) or (m.cos(a) == 0):
+        dy_dx.append(arb_val)
+    else:
+        dy_dx.append(((1 / (m.cos(a + small_val))) - (1 / m.cos(a))) / small_val)
+
 plt.plot(x, y)
+plt.plot(x, dy_dx)
 plt.ylim(-lim, lim)
 plt.show()
 
 # cosecant graph
+plt.title("f(x) = csc(x), f'(x) = -csc(x)cot(x)")
 y = []
 for i in range(u1, v1, step):
     a = deg_to_rad(i)
-
     if m.sin(a) == 0:
         y.append(arb_val)
     else:
         y.append(1 / m.sin(a))
 
+dy_dx = []
+for i in range(u1, v1, step):
+    a = deg_to_rad(i)
+    if (m.sin(a + small_val) == 0) or (m.sin(a) == 0):
+        dy_dx.append(arb_val)
+    else:
+        dy_dx.append(((1 / (m.sin(a + small_val))) - (1 / m.sin(a))) / small_val)
+
 plt.plot(x, y)
+plt.plot(x, dy_dx)
 plt.ylim(-lim, lim)
 plt.show()
 
 # cotangent graph
+plt.title("f(x) = cot(x), f'(x) = -csc²(x)")
 y = []
 for i in range(u1, v1, step):
     a = deg_to_rad(i)
-
     if m.tan(a) == 0:
         y.append(arb_val)
     else:
         y.append(1 / m.tan(a))
 
+dy_dx = []
+for i in range(u1, v1, step):
+    a = deg_to_rad(i)
+    if (m.tan(a + small_val) == 0) or (m.tan(a) == 0):
+        dy_dx.append(arb_val)
+    else:
+        dy_dx.append(((1 / (m.tan(a + small_val))) - (1 / m.tan(a))) / small_val)
+
 plt.plot(x, y)
+plt.plot(x, dy_dx)
 plt.ylim(-lim, lim)
 plt.show()
